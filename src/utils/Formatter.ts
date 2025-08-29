c5ecc7c3a7172bf1ee3f76ef1911a484df8c46a8
Mykyta <nikitaneklesa5@gmail.com>
2025-08-29 16:40:44 +0300

    created Formatter class

---
+++ src/utils/Formatter.ts
@@ -0,0 +1,22 @@
+export class Formatter {
+    private locale: string;
+
+    constructor(locale?: string) {
+        this.locale = locale ?? "en-US";
+    }
+
+    formatCurrency(amount: number, currency: string): string {
+        return new Intl.NumberFormat(this.locale, {
+            style: "currency",
+            currency,
+        }).format(amount);
+    }
+
+    formatDate(timestamp: number): string {
+        return new Intl.DateTimeFormat(this.locale, {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+        }).format(new Date(timestamp * 1000));
+    }
+}
